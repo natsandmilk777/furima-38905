eac1c28027b30d31e1e0c718b0b5bf3f148f004f
natsandmilk777 <natsandmilk777@gmail.com>
2023-06-17 15:07:15 +0900

    ユーザー管理機能5

--- db/migrate/20230606133043_devise_create_users.rb
+++ db/migrate/20230606133043_devise_create_users.rb
@@ -4,8 +4,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :nickname,           null: true, unique: true
-      t.string :email,              null: false, default: "", unique: true
+      t.string :nickname,           null: true
+      t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: "", unique: true
       t.string :last_name,          null: false
       t.string :first_name,         null: false
