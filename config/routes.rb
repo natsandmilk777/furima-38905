79c3b311750c29cac16a12642456be27c02eeb2c
natsandmilk777 <natsandmilk777@gmail.com>
2023-07-01 15:09:19 +0900

    商品出品機能８

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'items#index'
 
-
+  get '/items/new', to: 'items#new', as: 'new_item'
+  post '/items', to: 'items#create', as: 'items'
 end
