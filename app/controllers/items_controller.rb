13ea2891182f72190102a5cd5eb129ebb4abc63f
natsandmilk777 <natsandmilk777@gmail.com>
2023-07-01 14:04:40 +0900

    商品出品機能７

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,4 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
   #before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
