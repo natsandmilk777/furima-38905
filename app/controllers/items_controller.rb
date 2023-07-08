e85ca4a060d0383e3e7aa1418e50d838020a72c9
natsandmilk777 <natsandmilk777@gmail.com>
2023-07-08 16:46:30 +0900

    商品削除機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,10 +37,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if user_signed_in? && current_user.id == @item.user_id
+    if current_user.id == @item.user_id
       @item.destroy
-      redirect_to root_path
     end
+    redirect_to root_path
   end
 
   private
