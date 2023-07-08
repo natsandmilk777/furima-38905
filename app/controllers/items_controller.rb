045b3dbc2583b9bc1598f7f546a1312d913b23b0
natsandmilk777 <natsandmilk777@gmail.com>
2023-07-08 11:55:55 +0900

    updateアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,11 +30,11 @@ class ItemsController < ApplicationController
   end
 
   def update
-    #if @item.update(item_params)
-      #redirect_to item_path(@item.id)
-    #else
-      #render :edit
-    #end
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
